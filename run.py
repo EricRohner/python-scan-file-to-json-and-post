ddda683798ac4ca3b3a00c212a78e514e94b93f7
Eric Rohner <ericrohner22@gmail.com>
2021-11-21 21:42:26 -0700

    File readin and Json output working

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 import os
+import json
 import requests
 
 source = os.getcwd() + "/data/feedback"
 post_url = "www.example.com"
 
 def get_source_files():
-	return []
+	return os.listdir(source)
 
 def process_file_to_json(file):
-	return ""
+	with open("{}/{}".format(source,file)) as f:
+		lines = f.read().splitlines()
+		dict = {"title": lines[0], "name": lines[1], "date": lines[2], "feedback": lines[3:]}
+		return json.dumps(dict)
 
 def post_json(json):
 	return 200
@@ -20,4 +24,4 @@ if __name__ == "__main__":
 	for file in files:
 		json = process_file_to_json(file)
 		http_response = post_json(json)
-		print "{}, {}".format(file, http_response)
+		print ("{}, {}".format(file, http_response))
